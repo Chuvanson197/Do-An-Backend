38f2edc41d9421ca70ef11586833bc55a75019ff
nxa127 <nxa127@gmail.com>
2019-10-04 16:12:02 +0700

    authen api

---
+++ routes/authencation.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var router = express.Router();
+const request = require("request");
+
+router.post("/", function(req, res, next) {
+  if (req.body.google_access_token) {
+    request.post(
+      {
+        url: "http://timesheet.impl.vn/api/auth/google",
+        form: { token: req.body.google_access_token }
+      },
+      function(error, httpResponse, body) {
+        if (error) {
+          res.status(401).json({
+            msg: "Unauthorized, access denied"
+          });
+        } else {
+          res.json(JSON.parse(body));
+        }
+      }
+    );
+  } else {
+    res.status(401).json({
+      msg: "Invalid google access token!"
+    });
+  }
+});
+
+module.exports = router;
